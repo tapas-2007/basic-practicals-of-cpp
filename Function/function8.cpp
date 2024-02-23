31b13f8918294e2ca61528a618f1bdb889020dc4
tapas-2007 <tapusoni27@gmail.com>
2024-02-23 13:24:15 +0530

    active

---
+++ Function/function8.cpp
@@ -0,0 +1,19 @@
+//print  n number of sum buy using function.
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    int ans = 0;
+    for(int i=1;i<=n;i++){
+        ans += i;
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    
+    cout<< sum(n)<<endl;
+    return 0;
+}
