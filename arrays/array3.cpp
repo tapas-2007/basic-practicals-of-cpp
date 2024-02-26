a340ca12c966b7408281832a0aeeaf62dd8179e5
tapas-2007 <tapusoni27@gmail.com>
2024-02-26 20:20:15 +0530

    active

--- arrays/array3.cpp
+++ arrays/array3.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 int main(){
     int n;
+    cout<<"Enter number of student = ";
     cin>>n;
 
     string name[n];
+    int age[n];
     for(int i=0;i<n;i++){
-        cin>>name[i];
+        cin>>name[i]>>age[i];
     }
     
     for(int j=0;j<n;j++){
-        cout<<name[j]<<" ";
+        cout<<"Name ="<<name[j]<<" ";
+        cout<<"Age ="<<age[j]<<" "<<endl;
     }
     return 0;
 }
