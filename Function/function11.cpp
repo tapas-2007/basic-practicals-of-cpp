54ccc51fdc5bffab8f98669a03e6dbc64273ac5f
tapas-2007 <tapusoni27@gmail.com>
2024-03-04 09:23:25 +0530

    active

--- Function/function11.cpp
+++ Function/function11.cpp
@@ -40,8 +40,34 @@ int pow(int a,int b,int c){
     c=pow(a,b);
     return c;
 }
+int Det(int a,int b,int c,int d){
+int x,D,e,f,g,h,i;
+a:
+cout<<"Choise 1 for 2d determiner and 2 for 3d determiner = ";
+cin>>x;
+if (x==1) {
+cout<<"D= | a b |"<<endl;
+cout<<"   | c d |"<<endl;
+cout<<"Enter Value for a,b,c & d = ";
+cin>>a>>b>>c>>d;
+D = (a * d) - (b * c);
+return D;
+}
+else if (x==2) {
+cout<<"D= | a b c |"<<endl;
+cout<<"   | d e f |"<<endl;
+cout<<"   | g h i |"<<endl;
+cout<<"Enter Value for a,b,c,d,e,f,g,h,i = ";
+cin>>a>>b>>c>>d>>e>>f>>g>>h>>i;
+D=(a*e*i)+(b*d*h)+(c*g*f)-(d*e*h)-(f*g*c)-(i*d*e);
+return D;
+}
+else{
+    goto a;
+}
+}
 int main(){
-    int a,b,c,ch;
+    int a,b,c,ch,d;
     char x;
     menu:
     cout<<"1). Addition"<<endl;
@@ -51,7 +77,8 @@ int main(){
     cout<<"5). Modulo"<<endl;
     cout<<"6). Power"<<endl;
     cout<<"7). Square root"<<endl;
-    cout<<"8). Exit"<<endl;
+    cout<<"8). Determiners"<<endl;
+    cout<<"9). Exit"<<endl;
     cout<<endl;
     cout<<"Enter the choice = ";
     cin>>ch;
@@ -158,8 +185,21 @@ int main(){
             system("cls");
             return 0;
             }
-
+    
     case 8:
+    cout<<Det(a,b,c,d)<<endl;
+    cout<<endl;
+    cout<<"Choise y for continue or n for exit :";
+    cin>>x;
+    if(x=='y'||x=='Y'){
+        system("cls");
+        goto menu;
+    }else {
+        system("cls");
+        return 0;
+    }
+
+    case 9:
     system("cls");
     return 0;
 
