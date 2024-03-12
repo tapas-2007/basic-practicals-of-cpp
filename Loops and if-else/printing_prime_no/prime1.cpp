991e995d7a44b733abc033ad2536bcf3d0c11970
tapas-2007 <tapusoni27@gmail.com>
2024-03-12 14:34:13 +0530

    active

--- Loops and if-else/printing_prime_no/prime1.cpp
+++ Loops and if-else/printing_prime_no/prime1.cpp
@@ -5,7 +5,7 @@ int main() {
    int num;
    cout << "Enter a positive integer: ";
    cin >> num;
-   for(int i = 2; i <= num; i++) {
+   for(int i = 2; i*i <= num; i++) {
        if(num % i == 0) {
            cout << "Number is not prime" << endl;
            return 0;
